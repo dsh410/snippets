b9605ee68dbe242a5955e816e1ef5982a0e9ac11
dsh410 <dsh410@gmail.com>
2025-10-03 07:04:53 -0500

    set up prisma

---
+++ src/app/snippets/new/page.tsx
@@ -0,0 +1,43 @@
+import redirect from 'next/navigation';
+import { db } from '@/db';
+
+export default function SnippetCreatePage() {
+  async function createSnippet(data: FormData) {
+    'use server';
+    const title = data.get('title') as string;
+    const code = data.get('code') as string;
+
+    const snippet = await db.snippet.create({
+      data: {
+        title,
+        code,
+      },
+    });
+
+    console.log(snippet);
+  }
+  return (
+    <form action={createSnippet} className="max-w-xl mx-auto my-8">
+      <h3 className="font-bold m-3">Create a Snippet</h3>
+      <div className="flex flex-col gap-4">
+        <div className="flex gap-4">
+          <label className="w-12" htmlFor="title">
+            title
+          </label>
+          <input name="title" className="border rounded p-2 w-full" id="title" />
+        </div>
+
+        <div className="flex gap-4">
+          <label className="w-12" htmlFor="code">
+            Code
+          </label>
+          <textarea name="code" className="border rounded p-2 w-full" id="code" />
+        </div>
+
+        <button type="submit" className="rounded p-2 bg-blue-200">
+          Create
+        </button>
+      </div>
+    </form>
+  );
+}
